59700fb6e2c5e98042dbe94f587064c3e36eff52
Elavarasan <139960925+LElavarasan@users.noreply.github.com>
2023-07-21 13:00:08 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -7,7 +7,7 @@
    <body>
       <?php echo "Hello, This is Elavarasan ";?>
 <?php echo "Va macha va macha prakash";?>
-
+     <?php echo "hello i'm Varsan";?>
       <?php echo "Okay Bro";?>
    </body>
 
